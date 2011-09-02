6625e0b5ea7d49d61a0ef73fab8bec4daf7e533f
Fedor Gogolev <knsd@knsd.net>
2011-09-02 18:26:21 +0400

    setup.py pep8 fixes

--- setup.py
+++ setup.py
@@ -5,15 +5,20 @@ from distutils.extension import Extension
 
 VERSION = '0.0.1'
 
+DESCRIPTION = """\
+A gevent (http://www.gevent.org) adaption of the asynchronous MySQL driver
+from the Concurrence framework (http://opensource.hyves.org/concurrence)
+"""
+
 
 setup(
-    name = "gevent-MySQL",
-    version = VERSION,
-    license = "New BSD",
-    description = "A gevent (http://www.gevent.org) adaption of the asynchronous MySQL driver from the Concurrence framework (http://opensource.hyves.org/concurrence)",
-    package_dir = {'':'lib'},
-    packages = ['geventmysql'],
-    install_requires = ["gevent"],
-    ext_modules = [Extension("geventmysql._mysql",
+    name="gevent-MySQL",
+    version=VERSION,
+    license="New BSD",
+    description=DESCRIPTION,
+    package_dir={'': 'lib'},
+    packages=['geventmysql'],
+    install_requires=["gevent"],
+    ext_modules=[Extension("geventmysql._mysql",
         ["lib/geventmysql/geventmysql._mysql.c"])]
 )
